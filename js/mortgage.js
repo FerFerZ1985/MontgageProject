177624c5c8f52d130574420897ebf21629dce710
FerFerZ1985 <fernandosebastianzarate1985@gmail.com>
2025-09-12 12:35:02 -0300

    cambio de color en texto montototal

--- js/mortgage.js
+++ js/mortgage.js
@@ -1,12 +1,12 @@
 
-function calculateMortgage( event ) {
+function calculateMortgage(event) {
 
-    event.preventDefault ();
-    
-    let cuota = document.forms[ "fmortgage" ][ "fcuota" ].value;
-    let costoTotal = document.forms[ "fmortgage" ][ "fvalortotal" ].value;
-    let interes = document.forms[ "fmortgage" ][ "ftinteres" ].value;
-    let plazoAnio = document.forms[ "fmortgage" ][ "fplazo" ].value;
+    event.preventDefault();
+
+    let cuota = document.forms["fmortgage"]["fcuota"].value;
+    let costoTotal = document.forms["fmortgage"]["fvalortotal"].value;
+    let interes = document.forms["fmortgage"]["ftinteres"].value;
+    let plazoAnio = document.forms["fmortgage"]["fplazo"].value;
     const MONTHS_ON_YEAR = 12;
 
     const mortgage = {
@@ -19,26 +19,31 @@ function calculateMortgage( event ) {
     mortgage.costoTotalInmueble = costoTotal;
     mortgage.totalPrestamo = costoTotal - cuota;
     mortgage.totalInteres = mortgage.totalPrestamo * interes / 100;
-    mortgage.cuotaMensual = ( mortgage.totalPrestamo + mortgage.totalInteres ) / ( plazoAnio * MONTHS_ON_YEAR );
-    outputMortgage ( mortgage );
+    mortgage.cuotaMensual = (mortgage.totalPrestamo + mortgage.totalInteres) / (plazoAnio * MONTHS_ON_YEAR);
+    outputMortgage(mortgage);
 
-}  
-
-function outputMortgage ( finalMortgage ) {    
-    document.getElementById ( "omontoprestamo" ).innerHTML = valueToDollar ( finalMortgage.totalPrestamo );
-    document.getElementById ( "ocuota" ).innerHTML = valueToDollar ( finalMortgage.cuotaMensual );
-    var totalPrestamoPorcentaje = 0;
-    totalPrestamoPorcentaje = finalMortgage,totalPrestamo * 100 / finalMortgage.costoTotalInmueble;
-    alert (totalPrestamoPorcentaje);
 }
 
-function resetform(){
+function outputMortgage(finalMortgage) {
+    document.getElementById("omontoprestamo").innerHTML = valueToDollar(finalMortgage.totalPrestamo);
+    document.getElementById("ocuota").innerHTML = valueToDollar(finalMortgage.cuotaMensual);
+    var totalPrestamoPorcentaje = 0;
+    totalPrestamoPorcentaje = finalMortgage.totalPrestamo * 100 / finalMortgage.costoTotalInmueble;
+    alert(totalPrestamoPorcentaje);
+    if (totalPrestamoPorcentaje > 90) {
+        document.getElementById("omontoprestamo").className += " alertaporcentaje";
+    } else {
+        document.getElementById("omontoprestamo").className = "form-control";
+    }
+}
+
+function resetform() {
     document.forms["fmortgage"].reset();
 }
 
-function valueToDollar ( value ) {
-    const dollarformatter = new Intl.NumberFormat ( "en-US", { style:'currency' ,currency:'USD' ,minimumFractionDigits:2});
-    return dollarformatter.format ( value );
+function valueToDollar(value) {
+    const dollarformatter = new Intl.NumberFormat("en-US", { style: 'currency', currency: 'USD', minimumFractionDigits: 2 });
+    return dollarformatter.format(value);
 }
 
 
